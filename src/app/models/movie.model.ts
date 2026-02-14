8949039fcbc00be9c9ca1fd6107e45a41af86fd0
gtapiero <giovanny.tapiero@pi.edu.co>
2026-02-14 18:37:21 -0500

    commit - movie.models .

---
+++ src/app/models/movie.model.ts
@@ -0,0 +1,11 @@
+export interface Movie {
+    id: string;
+    title: string;
+    description: string;
+    director: string;
+    producer: string;
+    release_date: string;
+    running_time: string;
+    image: string;
+    movie_banner: string;
+}   
